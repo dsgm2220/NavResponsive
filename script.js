1b16c121c0740478253f245d40a9acfd956967cf
Dilan Gutierrez <123592764+dsgm2220@users.noreply.github.com>
2024-08-22 20:29:03 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// Toggle the navigation menu
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    // Animate Links
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }
+    });
+
+    // Burger Animation
+    burger.classList.toggle('toggle');
+});
+
+// Floating effect for the welcome text
+const welcomeText = document.querySelector('.floating');
+
+function floatText() {
+    welcomeText.style.transform = `translateY(${Math.sin(Date.now() / 1000) * 30}px)`;
+    requestAnimationFrame(floatText);
+}
+
+floatText();
